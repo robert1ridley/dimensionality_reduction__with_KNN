e310e1d43e54c7813137b903d693fb6e4e347978
Robert Ridley <robert.n.ridley@gmail.com>
2018-12-11 13:17:09 +0800

    svd working

--- models/Svd.py
+++ models/Svd.py
@@ -13,13 +13,15 @@ class Svd(object):
 
 
   def calc_svd(self):
-    U, S, V = np.linalg.svd(self.A, full_matrices=False)
+    U, S, V = np.linalg.svd(self.A, full_matrices=True)
     k = self.k
-    U_ = U[:,:k]
-    S_ = np.diag(S)[:k,:k]
-    V_ = V[:k,:k]
-    it = U_.dot(S_).dot(V_)
-    return it
+    sig_diag = np.zeros(shape=(k, k))
+    for i in range(k):
+      sig_diag[i, i] = S[i]
+
+    reduced_u = U[:len(U), :k]
+    reduced_s_v = np.dot(sig_diag, V[:k, :len(V)])
+    return np.dot(reduced_u, reduced_s_v)
 
 
 def get_data(filepath):
